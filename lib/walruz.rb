32673d37a13f70d725ec098426871b104caf6600
Roman Gonzalez <roman@noomi.com>
2009-06-04 10:49:20 -0700

    Adding the setup method to the Walruz class

--- lib/walruz.rb
+++ lib/walruz.rb
@@ -4,8 +4,26 @@ module Walruz
   end
   
   base_path = File.dirname(__FILE__)
-  autoload :Actor,   base_path + '/walruz/actor'   
-  autoload :Subject, base_path + '/walruz/subject' 
-  autoload :Policy,  base_path + '/walruz/policy'  
-  autoload :Utils,   base_path + '/walruz/utils'   
+  autoload :Actor,   base_path + '/walruz/actor'
+  autoload :Subject, base_path + '/walruz/subject'
+  autoload :Policy,  base_path + '/walruz/policy'
+  autoload :Utils,   base_path + '/walruz/utils'
+  
+  def self.setup
+    config = self.new
+    yield config
+  end
+  
+  def actors=(actors)
+    Array(actors).each do |actor|
+      actor.send(:include, Actor)
+    end
+  end
+  
+  def subjects=(subjects)
+    Array(subjects).each do |subject|
+      subject.send(:include, Subject)
+    end
+  end
+  
 end
