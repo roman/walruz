38d8d842bd249c4339cc0af58a550c5f9bcb6e9c
Roman Gonzalez <romanandreg@gmail.com>
2009-08-21 13:05:09 -0700

    Removing legacy specs from the Walruz::Subject module

--- spec/walruz/subject_spec.rb
+++ spec/walruz/subject_spec.rb
@@ -6,15 +6,4 @@ describe Walruz::Subject do
     Song.should respond_to(:check_authorizations)
   end
   
-  
-  describe "when executing validations on an invalid subject" do
-    
-    it "should raise an Walruz::AuthorizationActionsNotDefined error" do
-      lambda do
-        Beatle::JOHN.can_be?(:talk_with, Beatle::PAUL)
-      end.should raise_error(Walruz::AuthorizationActionsNotDefined)
-    end
-    
-  end
-  
 end
